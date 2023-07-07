99ec6d8a973bb5d1ca67d72c00763ba2f9566926
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-07-06 19:02:39 -0700

    no changes

--- start.js
+++ start.js
@@ -20,35 +20,29 @@ function getCharacterData(characterId, callback){
     connection.query(query, (err, results) => {
         if (err) {
             console.error('Error retrieving character data: ' + err.stack);
-            callback(err, null);
+            callback(err, null);  
         } else {
             callback(null, results[0]);
         }
     })
 }
-// process the user input and execute commands
-function processCommand(command){
 
-}
-// allows user to choose a direction
-function goCommand(direction){
 
-}
-// allows user to take an item
-function  takecommand(item){
-
+function selectOption(option){
 
 }
 // if the user is stuck
 function showHelp(){
 
 }
-// shows items and clothes and anything in inventory
-function showInventory(){
 
-}
+// shows items and clothes and anything in inventory
+function showInventory() {
+   
+  }
 // Start the server
 app.listen(3000, () => {
     console.log('Server started on port 3000');
   });
 
+startGame()
